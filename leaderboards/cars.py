8470f37ee57df1ba8f8c512842c2b706b39f484c
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-04-29 03:58:32 +0200

    create cars dictionnary for id / names

---
+++ leaderboards/cars.py
@@ -0,0 +1,67 @@
+dict_60s = {
+    0: "the esky v1",
+    1: "the meanie",
+    2: "la montaine",
+    3: "das 220",
+    4: "das 119i",
+    5: "le gorde",
+    6: "la regina",
+}
+dict_70s = {
+    0: "the esky v2", 
+    1: "il nonno 313",
+    2: "the rotary 3",
+    3: "la wedge",
+    4: "il cavallo 803",
+    5: "das 119e", 
+    6: "la hepta",
+    7: "the pebble v1",
+    8: "the pebble v2",
+    9: "the zetto",
+}
+dict_80s = {
+    0: "le cinq",
+    1: "das whip",
+    2: "turbo brick",
+    3: "das uberwhip",
+    4: "the cozzie sr5",
+    5: "the original",
+    6: "la super montaine",
+    7: "la longana",
+    8: "the gazelle",
+}
+dict_GroupB = {
+    0: "the 4r6",
+    1: "le 502",
+    2: "das hammer v1",
+    3: "das hammer v2",
+    4: "il gorilla 4s",
+    5: "the cozzie sr2",
+    6: "the cozzie sr71",
+    7: "the rotary b7",
+    8: "das hammer v3",
+    9: "il monster",
+    10: "il cavallo 882",
+    11: "le cinq b",
+    12: "das uberspeedvan",
+    13: "the king of africa",
+    14: "das 559",
+    15: "the hyena",
+}
+dict_GroupS = {
+    0: "das eibenhammer",
+    1: "the rotary s7",
+    2: "the umibozu",
+    3: "il gorilla e1",
+    4: "le 504",
+    5: "il gorilla e2",
+    6: "das superbaus",
+    7: "the t22",
+}
+dict_GroupA = {
+    0: "il gorillona",
+    1: "the fujin",
+    2: "the liftback",
+    3: "the max attack",
+    4: "the cozzie 90",
+}
