5e39785ee31ee4022daecce465f7a36551129d97
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-05-03 22:45:51 +0200

    add key send function (not tested yet)

--- self-driving/keys.py
+++ self-driving/keys.py
@@ -1,7 +1,11 @@
 import win32api
+from pynput.keyboard import Key, Controller
+
 
 # AZERTY keyboard
 keys = ['Z', 'Q', 'S', 'D']
+# output
+keyboard = Controller()
 
 
 def get_keys():
@@ -10,3 +14,13 @@ def get_keys():
         if win32api.GetAsyncKeyState(ord(keys[k])):
             array_out[k] = 1
     return array_out
+
+
+def set_keys(input):
+    for k in range(len(input)):
+        key = keys[k]
+        if input[k] == 1:
+            keyboard.press(key)
+        else:
+            keyboard.release(key)
+    return
