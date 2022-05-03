243ef049f75ead9252a021a69f4a3ca8ee18382d
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-05-03 22:46:27 +0200

    add memory acquisition and all data view using putText

--- self-driving/data_acquisition.py
+++ self-driving/data_acquisition.py
@@ -3,7 +3,19 @@ import numpy as np
 import mss
 import screen
 import keys
+import dashboard.dashboard as dashboard
 
+
+# memory data
+pm = dashboard.open_aor_process()
+addresses = dashboard.get_addresses_selection('cheat-table/artofrally.CT', pm, ['Speed pointer 1', 'Steering pointer 1'])
+print(addresses)
+addresses_pointer = []
+addresses_type = []
+
+for k in addresses.keys():
+    addresses_pointer.append(addresses[k][0])
+    addresses_type.append(addresses[k][1])
 
 # move window for acquisition
 screen.move_window_0('art of rally')
@@ -13,6 +25,7 @@ print('prepare for data acquisition')
 # data acquired
 data_images = []
 data_keys = []
+data_memory = []
 
 # time before acquisition
 time.sleep(5)
@@ -21,10 +34,14 @@ print('data acquisition begin')
 
 with mss.mss() as sct:
     i = 0
-    while i < 30:
+    while i < 300:
         data_images.append(screen.get_img(sct, 112, 512, 50, 384))
         data_keys.append(keys.get_keys())
-        time.sleep(0.5)
+        mem = []
+        for j in range(len(addresses_pointer)):
+            mem.append(dashboard.get_value(addresses_pointer[j], addresses_type[j], pm))
+        data_memory.append(mem)
+        time.sleep(0.1)
         i += 1
 
 print('data acquisition ended')
@@ -33,9 +50,10 @@ codename = str(int(time.time()))
 codename = 'test'
 data_images = np.array(data_images)
 data_keys = np.array(data_keys)
-
+data_memory = np.array(data_memory)
 
 np.save(f'./self-driving/data/{codename}_images', data_images)
 np.save(f'./self-driving/data/{codename}_keys', data_keys)
+np.save(f'./self-driving/data/{codename}_memory', data_memory)
 
 print('data saved')
--- self-driving/data_view.py
+++ self-driving/data_view.py
@@ -1,12 +1,42 @@
+import time
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 
 codename = 'test'
 
 data_images = np.load(f'./self-driving/data/{codename}_images.npy')
 data_keys = np.load(f'./self-driving/data/{codename}_keys.npy')
+data_memory = np.load(f'./self-driving/data/{codename}_memory.npy')
 
 print(data_images.shape)
-plt.imshow(cv2.cvtColor(data_images[0, :, :, 0:3], cv2.COLOR_BGR2RGB))
-plt.show()
+print(data_keys.shape)
+print(data_memory.shape)
+
+for i in range(len(data_images)):
+    img = data_images[i, :, :]
+    keys = data_keys[i]
+    memory = data_memory[i]
+    cv2.putText(img, f'Z', (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+    cv2.putText(img, f'Q', (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+    cv2.putText(img, f'S', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+    cv2.putText(img, f'D', (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+    if keys[0] == 1:
+        cv2.putText(img, f'Z', (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    if keys[1] == 1:
+        cv2.putText(img, f'Q', (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    if keys[2] == 1:
+        cv2.putText(img, f'S', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    if keys[3] == 1:
+        cv2.putText(img, f'D', (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    cv2.putText(img, f'{str(round(memory[0], 2)).rjust(6)}', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    if memory[1] >= 0:
+        cv2.putText(img, f'+{str(round(memory[1], 2))}', (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    else:
+        cv2.putText(img, f'-{str(round(memory[1], 2))[1:]}', (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+    cv2.imshow("data_view", img)
+
+    if cv2.waitKey(1) & 0Xff == ord('q'):
+        break
+
+    time.sleep(0.01)
+cv2.destroyAllWindows()  # close all windows
