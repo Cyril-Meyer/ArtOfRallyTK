c6068d4a22f191f03d1a75adb9ab7caa987129c1
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-08-06 01:19:03 +0200

    update logger with actually logging system

--- aorc-logger/server.py
+++ aorc-logger/server.py
@@ -1,7 +1,11 @@
+import time
+import threading
 import eventlet
 import eventlet.wsgi
 import socketio
 
+default_output = open('output.csv', 'w')
+output = default_output
 LOGGER = False
 sio = socketio.Server(logger=LOGGER)
 app = socketio.WSGIApp(sio)
@@ -33,7 +37,29 @@ def waypointsGathered(sid, data):
 
 @sio.event(namespace='/users')
 def stageUpdate(sid, data):
+    global output
+    # velocity = velocity * 0.6 * factor
+    # factor = 3.6 for kmh, 2.237 for mph
+    output.write(f"{str(time.time_ns() // 1000000)}," +
+                 f"{round(float(data['carData']['drivetrain']['velocity']*0.6*3.6),2)}," +
+                 f"{round(float(data['carData']['drivetrain']['rpm']),2)}," +
+                 f"{int(data['carData']['drivetrain']['gear'])}\n")
+    return
     print('stageUpdate', sid, data)
 
 
+def record_30s():
+    global output
+    while True:
+        filename = input()
+        output = open(f'{filename}.csv', 'w')
+        output.write('time,speed,rpm,gear\n')
+        print(f'>{filename}.csv')
+        time.sleep(30)
+        print(f'<{filename}.csv')
+        output.close()
+        output = default_output
+
+
+threading.Thread(target=record_30s,).start()
 eventlet.wsgi.server(eventlet.listen(('', 4593)), app)
